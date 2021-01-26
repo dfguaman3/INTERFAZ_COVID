eb0ac044f7b2fdc895b3e22ac305f892a62e19b2
danny <dannyfabricio1990@hotmail.com>
2021-01-25 19:54:01 -0500

    comit3

--- lectura.py
+++ lectura.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
-data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv", parse_dates=['date'], index_col='date')
+import requests,json,urllib
+url1='public/data/owid-covid-data.csv'
+url='https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.json'
+#data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv", parse_dates=['date'], index_col='date')
 
-print(data.location)
+try:
+    response = urllib.request.urlopen(url).read().decode()
+    datos = json.dumps(response)
+    datos2=json.loads(datos)
+    
+    print(datos2['ALB']['data'][3])
+    #print(datos['ALB']['data'][3]['date'])
+    
+    
+    
+except Exception as e:
+    print(e)
--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from mplwidget import MplWidget
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
-
+url='https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.json'
 qtCreatorFile = "interfaz.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
-
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
@@ -16,40 +15,64 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.MplWidget = MplWidget(self.MplWidget)
         self.cargar.clicked.connect(self.getCSV)
-        
+        self.graficar.clicked.connect(self.presentarTabla)
         self.df = pd.read_csv('./data/covid_data.csv', parse_dates=['Country'], sep=',', na_values='')
-        
         
     def getCSV(self):
         
         paises = self.df['Country']
         estados = self.df['State']
-        fechas = self.df.loc['1/22/2020':'10/31/2020']
-        col_names=self.df.columns.tolist()
+        fechas=self.df.columns.tolist()
+        #fechas = self.df.loc['1/22/2020':'10/31/2020']
+        
         dat=pd.DataFrame(self.df)
+        
         #fechas.groupby('Country')[col_names[2]].sum().plot(kin='bar',legend='Reverse')
-        print(len(col_names))
+        
         for i in paises:
             self.listacountry.addItem(str(i))
         for j in estados:
             self.listastate.addItem(str(j))   
         
-
     def presentarTabla(self):
         
         try:
-            titulo='Daily Number of Cases and Deaths in '
-            self.titulo=titulo
-            data=self.df.loc['1/22/2020':'10/31/2020']
-            self.lon=self.df.columns.tolist()
+            x_name = 'Daily Number of Cases and Deaths in Place'
+            y_name = 'Date'
+            z_name = 'Tiempo'
+            self.x = x_name
+            self.y = y_name
+            self.z = z_name
+            lista_mag = []
+            lista_place =[]
+            lista_time = []
+            response = urllib.request.urlopen(url).read().decode()
+            data = json.loads(response)
+            long = len(data['data'])
+            for i in range(long): 
+                
+                mag = data['data'][i]['date']['mag']#float
+                place = data['data'][i]['total_cases']['place']#string
+                lista_mag += [mag]
+                lista_place += [place]
             
+            
+            self.lon = range(1,len(lista_mag)+1)
 
+            for row in range(len(lista_mag)):
+                contentCell = str(lista_mag[row])
+                self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(contentCell))
+                contentCell = str(lista_place[row])
+                self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(contentCell))
+                contentCell = str(lista_time[row])
+                self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(contentCell))
+            
             self.MplWidget.canvas.axes.clear()
-            self.MplWidget.canvas.axes.bar(list(self.lon),data, width = 0.4, align='center')
-            self.MplWidget.canvas.axes.legend((self.titulo, self.titulo), loc='upper right')
-            self.MplWidget.canvas.axes.set_title(f'{self.titulo}')
-            self.MplWidget.canvas.axes.set_xlabel('Fecha')
-            self.MplWidget.canvas.axes.set_ylabel('Numero de casos diarios')
+            self.MplWidget.canvas.axes.bar(list(self.lon),lista_mag, width = 0.4, align='center')
+            self.MplWidget.canvas.axes.legend((self.x, self.y), loc='upper right')
+            self.MplWidget.canvas.axes.set_title(f'{self.x} - {self.y}')
+            self.MplWidget.canvas.axes.set_xlabel('Numero de sismos')
+            self.MplWidget.canvas.axes.set_ylabel('Magnitud (Ritcher Scale)')
             self.MplWidget.canvas.axes.grid(True)
             self.MplWidget.canvas.draw()
         except Exception as e:

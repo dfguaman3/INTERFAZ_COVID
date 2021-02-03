18a7ad14e4b9740008bbbc07de57722fd36e17b1
danny <dannyfabricio1990@hotmail.com>
2021-02-03 18:30:08 -0500

    comit7

--- main.py
+++ main.py
@@ -18,18 +18,19 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.casos.setChecked(True)
         self.listacountry.itemClicked.connect(self.aux)
         self.df = pd.read_csv('./data/covid_data2.csv', parse_dates=['Country'], sep=',', na_values='')
-        
-        datos=pd.DataFrame(self.df, columns= ['Country','1/22/2020'])
         self.country_selected=''
-        self.index_country=None   
+           
     def getCSV(self):
-        
+        cases=[]
+        deaths=[]
+        tipo=[]
         paises = self.df['Country']
         estados = self.df['State']
         dias=self.df.columns.tolist()
         self.fechas = self.df.loc['1/22/2020':'2/5/2020']
         self.fechas2 = self.fechas.columns.tolist()
-        
+        print(self.fechas.values[0][0])
+        print(len(self.fechas))
         for i in paises:
             self.listacountry.addItem(str(i))
             self.extrac_country=self.listacountry.currentItem()
@@ -37,23 +38,31 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             
         for j in estados:
             self.listastate.addItem(str(j))
-        cases=[]
-        deaths=[]
-        tipo=[]
-        for k in range(2,15):
-            valores=self.fechas.values[6]#Australia
+        '''
+        for l in range(2,len(self.fechas)):
+            if str(self.country_selected)==str(self.fechas.values[0][0]):
+                valores=self.fechas.values[l]
+                for k in range(2,15):
+                    casos=valores[k].split(sep=" ")
+                    contagios=[casos[0]]
+                    muertes=[casos[2]]
+                    pais=valores[0]
+                    cases+=contagios
+                    deaths+=muertes
             
+        '''
+        for k in range(2,15):
+            valores=self.fechas.values[10]#Australia
             casos=valores[k].split(sep=" ")
             contagios=[casos[0]]
             muertes=[casos[2]]
             pais=valores[0]
             cases+=contagios
-            
             deaths+=muertes
             
         print(cases)
         print(deaths)
-            
+        
         self.lon = range(1,len(dias)+1)
         
         x_name = 'Daily Number of Cases and Deaths in '
@@ -61,22 +70,21 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         z_name = 'Tiempo'
         self.x = x_name
         self.y = y_name
-        self.z = z_name
+        
         if self.casos.isChecked():
             tipo=cases
         elif self.muertes.isChecked():
             tipo=deaths
             
-
         self.MplWidget.canvas.axes.clear()
-        self.MplWidget.canvas.axes.bar(list(self.fechas2[2:15]),tipo, width = 1, align='center')
+        self.MplWidget.canvas.axes.bar(list(self.fechas2[2:15]),tipo, width = 0.2, align='center')
         self.MplWidget.canvas.axes.legend((self.x, self.y), loc='upper right')
         self.MplWidget.canvas.axes.set_title(f'{self.x} - {self.y}')
         self.MplWidget.canvas.axes.set_xlabel('Fecha')
         self.MplWidget.canvas.axes.set_ylabel('Numero de casos')
         self.MplWidget.canvas.axes.grid(True)
         self.MplWidget.canvas.draw()
-        
+
     def aux(self,extrac_country):
         self.country_selected=str(extrac_country.text())
         
